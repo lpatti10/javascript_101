1b94717b8b1e26a698b70c67aa861aa5b8cf2ee4
Laura Patti <laurapatti@Lauras-MacBook-Pro.local>
2014-06-10 17:01:25 -0400

    Completed split and concat

---
+++ lp_scripts.js
@@ -0,0 +1,53 @@
+var firstname = 'Laura';
+var lastname = 'Patti';
+var car = 'Volvo';
+var favnum = 10;
+var pet = 'Maya';
+
+console.log(firstname, lastname, car, favnum, pet);
+
+var animal ='Dogs are the best';
+var tree = 'Pine trees';
+var city = 'Atlanta';
+
+animal.split(" ", 10);
+tree.split(" ", 10);
+city.split(" ", 10);
+
+
+animal.concat(tree, city);
+tree.concat(animal, city);
+city.concat(animal, tree);
+
+
+
+
+
+
+
+
+
+/*
+console.log( );
+
+
+
+var splits = animal.split(" ", 10);
+
+print(splits);
+
+
+
+
+
+.split()
+.concat()
+.join()
+.length
+
+
+var listvariables = function (firstname, lastname, car, favnum, pet) {
+ console.log("These are my five variables:" + " " + listvariables);
+};
+
+*/
