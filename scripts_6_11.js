5f8a1bf57efd0c2938a1ae6fc0ddd1ce8e60339c
Laura Patti <laurapatti@Lauras-MacBook-Pro.local>
2014-06-11 17:02:42 -0400

    New homework assignment file

---
+++ scripts_6_11.js
@@ -0,0 +1,14 @@
+// Simple addition operation returns value 102
+64 + 38; 
+// Simple subtraction operation returns value -176
+28 - 204;
+// Simple multiplication operation returns value 28
+14 * 2;
+// Simple division operation returns value 7
+77 / 11;
+// Looks like spaces are optional, returns value 7 
+77/11;
+//Complex operation returns value 404
+25 + 488 + 11 - 20 * 12 / 2;
+//ERROR, not sure if = operator has a use with simple operations...
+55=;
