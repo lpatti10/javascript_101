f510bed7321c70f3373ef7e75d009244bce58795
Laura Patti <laurapatti@Lauras-MacBook-Pro.local>
2014-06-11 17:26:25 -0400

    Added src link in index footer

--- scripts_6_11.js
+++ scripts_6_11.js
@@ -12,3 +12,29 @@
 25 + 488 + 11 - 20 * 12 / 2;
 //ERROR, not sure if = operator has a use with simple operations...
 55=;
+
+//Declaring variable "employer" as GE
+var employer = "GE"
+//If (condition is true) {statement appears}
+if (employer = "GE") {
+	console.log("You sold out.")
+}
+//If not, then this statement appears
+else {
+	console.log("Who's that?")
+}
+//Tested this by entering <employer=> into console and it returns "GE". Then entered 
+
+/*
+var userChoice = prompt(“Do you choose rock, paper or scissors?”);
+var computerChoice = Math.random();
+console.log(computerChoice);
+if(computerChoice < 0.33) {
+console.log(“rock”);
+}
+else if(computerChoice >= 0.33 && computerChoice <= 0.67) {
+console.log(“paper”);
+}
+else {
+console.log(“scissors”);
+}
