ec657e8b63a2910f44f5700b48f412ba112a3dfc
Laura Patti <laurapatti@Lauras-MacBook-Pro.local>
2014-06-13 13:22:33 -0400

    Progress to problem 2

--- 06_12_hmwk02.js
+++ 06_12_hmwk02.js
@@ -19,6 +19,17 @@
       true
       14 false  
   (!)TAKEAWAY: this returned all results (true and false) for "GBP"
+      
+02c | Tried an additional console.log to isolate true result
+      var currencyPull = ["USD", "USD", "USD", "USD", "USD", "USD", "USD", "USD", "USD", "USD", "GBP", "USD", "USD", "USD", "USD", "USD", "USD", "USD", "USD", "USD", "USD", "USD", "USD", "USD", "USD"]
+      currencyPull.forEach(function (item) {
+      console.log(item === "GBP");
+      });
+      console.log(true);
+  (FAIL)...and got this:
+      10 false
+      true
+      14 false  
 
 
 
